2e7a4b8c1313914a5e0edc5005d1c978f59fab7e
Satish Junghare <satish8264@gmail.com>
2019-04-12 01:54:15 +0530

    first commit

---
+++ numbers.py
@@ -0,0 +1,13 @@
+a = 1
+b = 2
+c = 3
+d = "d"
+print(a+b+c)
+
+# Get variable value type
+print(type(a))
+print(type(d))
+
+# We cannot add string + int value
+# To add string value with int, we can use int() to convert string to int
+
---
+++ variables.py
@@ -0,0 +1,13 @@
+# Variables
+user_name = input("What is your name? ")
+user_age = input("What is your age "+user_name+" ? ")
+print("So "+user_name+", you are "+user_age+" years old.")
+
+# Rules ( Can )
+# ---------
+# - Can start with underscore
+# - Can use numbers in between or at the end
+# -
+# Rules ( Can not )
+# - Can not start with number
+# -
