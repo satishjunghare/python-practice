47de987ae3d2d5d0ecca99bdc4b584280a1504c1
Satish Junghare <satish8264@gmail.com>
2019-04-14 01:19:41 +0530

    python basics

---
+++ exceptionHandling.py
@@ -0,0 +1,7 @@
+def divide(a,b):
+    try:
+        return a/b
+    except Exception as e:
+        return e
+
+print(divide(10,0))
---
+++ loops.py
@@ -0,0 +1,5 @@
+emails = ["satish@gmail.com","rajan@hotmail.com","ranjith@gmail.com","anas@ewebcore.net"]
+
+for email in emails:
+    if 'satish' in email:
+        print(email)
