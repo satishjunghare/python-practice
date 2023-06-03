90d33e659d8750a906cce0bb9adf0eeb1b605ded
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 <satish8264@gmail.com>
2023-06-03 12:19:19 +0530

    progressbar, yield

--- progressBar.py
+++ progressBar.py
@@ -2,27 +2,65 @@ import time
 import sys
 import click
 
-def yieldfun():
+def hotelImport():
+    time.sleep(1)
+    yield 10
+    
+    time.sleep(1)
+    yield 20
+    
+    time.sleep(1)
+    yield 30
+    
+    time.sleep(1)
+    yield 40
+    
+    time.sleep(1)
+    yield 50
+    
+    time.sleep(1)
+    yield 60
+    
+    time.sleep(1)
+    yield 70
+    
+    time.sleep(1)
+    yield 80
+    
+    time.sleep(1)
+    yield 90
+    
     time.sleep(1)
     yield 100
 
-    time.sleep(1)
-    yield 100
+def progress_bar(iteration, total, prefix='', suffix='', length=30, fill='█', print_end='\r'):
+    """
+    Print a progress bar to the console.
+    
+    Parameters:
+    - iteration: Current iteration (int)
+    - total: Total iterations (int)
+    - prefix: Prefix string (str)
+    - suffix: Suffix string (str)
+    - length: Length of the progress bar in characters (int)
+    - fill: Fill character for the progress bar (str)
+    - print_end: End character for the print statement (str)
+    """
+    percent = f'{100 * (iteration / float(total)):.1f}'
+    filled_length = int(length * iteration // total)
+    bar = fill * filled_length + '-' * (length - filled_length)
+    print(f'\r{prefix} |{bar}| {percent}% {suffix}', end=print_end)
+    
+# Example usage
+for i in hotelImport():
+    progress_bar(i, 100, prefix='Syncing Vervotech Hotels:', suffix='Complete')
 
-    time.sleep(1)
-    yield 1000
+# Add a newline after the progress bar is complete
+print()
 
-    time.sleep(1)
-    yield 100000
+for i in hotelImport():
+    progress_bar(i, 100, prefix='Syncing Vervotech Room:', suffix='Complete')
 
-    time.sleep(2)
-    yield 1000000
+print()
 
-# with click.progressbar(range(1000000)) as bar:
-#     for i in bar:
-#         time.sleep(0.1)
-#         pass
-
-with click.progressbar(all_the_users_to_process) as bar:
-    for user in bar:
-        modify_the_user(user)
+print("Syncing Complete!")
